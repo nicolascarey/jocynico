4492f3636908dec3429027ba1c7e79a3acdfc32e
nsdcarey <nicolas@potatolondon.com>
2020-05-31 17:30:31 -0700

    content in progress

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  theme: {
+  	fontFamily: {
+		'display': ['Prata', 'Georgia'],
+		'sans' : ['Tenor Sans', 'Arial'],
+  	},
+    colors: {
+      ligDark: '#3E545B',
+      ligMid: '#678B97',
+      ligLight: '#9CBCC7',
+      cloudDark: '#B2C8CF',
+      cloudLight: '#D8E3E6',
+      tierraDark: '#AB7F60',
+      tierraLight: '#D2B274',
+      grassDark: '#497F51',
+      grassLight: '#629D65',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
